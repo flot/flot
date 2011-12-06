4006d3f22e8786ddeca48c6d2d6bc7ebd3cfaa7c
Stanislav Ievlev <stanislav.ievlev@gmail.com>
2011-12-06 15:04:14 +0400

    improve padding for labels with angle > PI/2

--- jquery.flot.js
+++ jquery.flot.js
@@ -1734,7 +1734,7 @@
                             x = plotOffset.left + axis.p2c(tick.v);
                             y = box.top + 2 * box.padding;
                             if(angle > Math.PI/2) {
-                                x -= line.width;
+                                x -= line.width - 2* box.padding;
                                 y += line.height - 2* box.padding;
                                 angle += Math.PI;
                             }
