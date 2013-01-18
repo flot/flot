3b2d43bf65f773c838a89b1645d7c8e741a7908d
David Schnur <dnschnur@gmail.com>
2013-01-18 16:45:28 -0500

    Provide a way for plugins to override classes.

--- jquery.flot.js
+++ jquery.flot.js
@@ -541,9 +541,16 @@ Licensed under the MIT license.
         }
 
         function initPlugins() {
+
+            // References to key classes, allowing plugins to modify them
+
+            var classes = {
+                Canvas: Canvas
+            };
+
             for (var i = 0; i < plugins.length; ++i) {
                 var p = plugins[i];
-                p.init(plot);
+                p.init(plot, classes);
                 if (p.options)
                     $.extend(true, options, p.options);
             }
