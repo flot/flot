0f83ac3d5cecc10d6616f1eeb1a2e23fdac803da
David Schnur <dnschnur@gmail.com>
2013-04-11 20:37:02 -0400

    Fixed missing semicolons.

--- jquery.flot.js
+++ jquery.flot.js
@@ -410,7 +410,7 @@ Licensed under the MIT license.
 			if (layerCache != null) {
 				for (var styleKey in layerCache) {
 					if (hasOwnProperty.call(layerCache, styleKey)) {
-						var styleCache = layerCache[styleKey]
+						var styleCache = layerCache[styleKey];
 						for (var key in styleCache) {
 							if (hasOwnProperty.call(styleCache, key)) {
 								styleCache[key].active = false;
@@ -1100,7 +1100,7 @@ Licensed under the MIT license.
             // second pass: find datamax/datamin for auto-scaling
             for (i = 0; i < series.length; ++i) {
                 s = series[i];
-                points = s.datapoints.points,
+                points = s.datapoints.points;
                 ps = s.datapoints.pointsize;
                 format = s.datapoints.format;
 
@@ -2970,7 +2970,7 @@ Licensed under the MIT license.
         return this.each(function() {
             $.plot(this, data, options);
         });
-    }
+    };
 
     // round to nearby lower multiple of base
     function floorInBase(n, base) {
