7f5f90384ed6d6c30b232580d358c84e355919ab
David Schnur <dnschnur@gmail.com>
2013-05-22 08:17:57 -0400

    Updated version number to 0.8.1 final.

--- jquery.flot.js
+++ jquery.flot.js
@@ -1,4 +1,4 @@
-/* Javascript plotting library for jQuery, version 0.8.1-alpha.
+/* Javascript plotting library for jQuery, version 0.8.1.
 
 Copyright (c) 2007-2013 IOLA and Ole Laursen.
 Licensed under the MIT license.
@@ -3041,7 +3041,7 @@ Licensed under the MIT license.
         return plot;
     };
 
-    $.plot.version = "0.8.1-alpha";
+    $.plot.version = "0.8.1";
 
     $.plot.plugins = [];
 
