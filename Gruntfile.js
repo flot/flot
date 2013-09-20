c5f6d29e6255e6416a048d3103f8a94c6f09b0af
Nick Schonning <nschonni@gmail.com>
2013-09-20 14:12:00 -0400

    Use JSHint dot file settings in Grunt

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,60 +1,44 @@
 /*jshint node: true */
 module.exports = function(grunt) {
 
-    // Project configuration.
-    grunt.initConfig({
-        // Metadata.
-        pkg: grunt.file.readJSON('package.json'),
-        banner: '/*! <%= pkg.name %> - v<%= pkg.version %> - ' +
-            '* Copyright (c) <%= grunt.template.today("yyyy") %> IOLA and <%= pkg.author.name %>;' +
-            ' Licensed <%= pkg.license %> */\n',
-        // Task configuration.
-        uglify: {
-            options: {
-                banner: '<%= banner %>'
-            },
-            dist: {
+	// Project configuration.
+	grunt.initConfig({
+		// Metadata.
+		pkg: grunt.file.readJSON('package.json'),
+		banner: '/*! <%= pkg.name %> - v<%= pkg.version %> - ' +
+			'* Copyright (c) <%= grunt.template.today("yyyy") %> IOLA and <%= pkg.author.name %>;' +
+			' Licensed <%= pkg.license %> */\n',
+		// Task configuration.
+		uglify: {
+			options: {
+				banner: '<%= banner %>'
+			},
+			dist: {
 				expand: true,
 				flatten: true,
-                src: ['jquery.*.js', '!jquery.js'],
-                dest: 'dist/',
+				src: ['jquery.*.js', '!jquery.js'],
+				dest: 'dist/',
 				rename: function(base, path) {
 					return base + path.replace(/\.js/, '.min.js');
 				}
-            }
-        },
-        jshint: {
-            options: {
-                curly: true,
-                eqeqeq: true,
-                immed: true,
-                latedef: true,
-                newcap: true,
-                noarg: true,
-                sub: true,
-                undef: true,
-                unused: true,
-                boss: true,
-                eqnull: true,
-                browser: true,
-                globals: {
-                    jQuery: true
-                }
-            },
-            gruntfile: {
-                src: 'Gruntfile.js'
-            },
-            flot: {
-                src: ['jquery.flot*.js', '!exacanvas.js']
-            }
-        }
-    });
+			}
+		},
+		jshint: {
+			options: grunt.file.readJSON('.jshintrc'),
+			gruntfile: {
+				src: 'Gruntfile.js'
+			},
+			flot: {
+				src: ['jquery.*.js']
+			}
+		}
+	});
 
-    // These plugins provide necessary tasks.
-    grunt.loadNpmTasks('grunt-contrib-uglify');
-    grunt.loadNpmTasks('grunt-contrib-jshint');
+	// These plugins provide necessary tasks.
+	grunt.loadNpmTasks('grunt-contrib-uglify');
+	grunt.loadNpmTasks('grunt-contrib-jshint');
 
-    // Default task.
-    grunt.registerTask('default', ['jshint', 'uglify']);
+	// Default task.
+	grunt.registerTask('default', ['jshint', 'uglify']);
 
 };
