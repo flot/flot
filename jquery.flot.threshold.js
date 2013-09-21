ce18ff8a06ce7a53280f11f647a1743828476f5f
Nick Schonning <nschonni@gmail.com>
2013-09-20 22:18:14 -0400

    jQuery styleguide Flot Threshold
    - Double quotes
    - variable hoisting

--- jquery.flot.threshold.js
+++ jquery.flot.threshold.js
@@ -46,10 +46,13 @@ You may need to check for this in hover events.
     var options = {
         series: { threshold: null } // or { below: number, color: color spec}
     };
-    
+
     function init(plot) {
         function thresholdData(plot, s, datapoints, below, color) {
-            var ps = datapoints.pointsize, i, x, y, p, prevp,
+            var addCrossingPoints, i, interx, m, origIndex, origpoints, p, prevp, x, y,
+                threspoints = [],
+                newpoints = [],
+                ps = datapoints.pointsize,
                 thresholded = $.extend({}, s); // note: shallow copy
 
             thresholded.datapoints = { points: [], pointsize: ps, format: datapoints.format };
@@ -58,13 +61,11 @@ You may need to check for this in hover events.
             thresholded.threshold = null;
             thresholded.originSeries = s;
             thresholded.data = [];
- 
-            var origpoints = datapoints.points,
-                addCrossingPoints = s.lines.show;
 
-            var threspoints = [];
-            var newpoints = [];
-            var m;
+            origpoints = datapoints.points;
+            addCrossingPoints = s.lines.show;
+
+
 
             for (i = 0; i < origpoints.length; i += ps) {
                 x = origpoints[i];
@@ -76,10 +77,10 @@ You may need to check for this in hover events.
                 } else {
                     p = newpoints;
                 }
-                
-                if (addCrossingPoints && prevp != p && x != null && i > 0 &&
+
+                if (addCrossingPoints && prevp !== p && x != null && i > 0 &&
                     origpoints[i - ps] != null) {
-                    var interx = x + (below - y) * (x - origpoints[i - ps]) / (y - origpoints[i - ps + 1]);
+                    interx = x + (below - y) * (x - origpoints[i - ps]) / (y - origpoints[i - ps + 1]);
                     prevp.push(interx);
                     prevp.push(below);
                     for (m = 2; m < ps; ++m) {
@@ -107,26 +108,26 @@ You may need to check for this in hover events.
 
             datapoints.points = newpoints;
             thresholded.datapoints.points = threspoints;
-            
+
             if (thresholded.datapoints.points.length > 0) {
-                var origIndex = $.inArray(s, plot.getData());
+                origIndex = $.inArray(s, plot.getData());
                 // Insert newly-generated series right after original one (to prevent it from becoming top-most)
                 plot.getData().splice(origIndex + 1, 0, thresholded);
             }
-                
+
             // FIXME: there are probably some edge cases left in bars
         }
-        
+
         function processThresholds(plot, s, datapoints) {
             if (!s.threshold) {
                 return;
             }
-            
+
             if (s.threshold instanceof Array) {
                 s.threshold.sort(function(a, b) {
                     return a.below - b.below;
                 });
-                
+
                 $(s.threshold).each(function(i, th) {
                     thresholdData(plot, s, datapoints, th.below, th.color);
                 });
@@ -135,14 +136,14 @@ You may need to check for this in hover events.
                 thresholdData(plot, s, datapoints, s.threshold.below, s.threshold.color);
             }
         }
-        
+
         plot.hooks.processDatapoints.push(processThresholds);
     }
-    
+
     $.plot.plugins.push({
         init: init,
         options: options,
-        name: 'threshold',
-        version: '1.2'
+        name: "threshold",
+        version: "1.2"
     });
 })(jQuery);
