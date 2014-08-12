bb86cb4742ec72e2d9114e49b604ea74b0c28848
Ciprian Ceteras <cipix2000@gmail.com>
2014-08-12 15:03:59 +0300

    import jasmine

---
+++ spec/log_axis_Spec.js
@@ -0,0 +1,10 @@
+/* global describe, it, expect*/
+
+describe("tests for log scale functions", function () {
+
+  it('should use linear scale for low dynamic range intervals');
+
+  it('should use log scales for high dynamic range intervals', function () {
+    expect(true).toBe(true);
+  });
+});
