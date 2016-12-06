1fde83ff26c0bbd82e8898c841af124c46528c6f
Ciprian Ceteras <cipix2000@gmail.com>
2016-12-06 18:51:32 +0200

    update time support and add support for fast analogWaveforms

--- jquery.flot.flatdata.js
+++ jquery.flot.flatdata.js
@@ -8,9 +8,11 @@ Set series.flatdata to true to enable.
 (function ($) {
     function process1DRawData(plot, series, data, datapoints) {
         if (series.flatdata === true) {
+            var start = series.start || 0;
+            var step = series.step || 1;
             datapoints.pointsize = 2;
             for (var i = 0, j=0; i < data.length; i++, j+=2) {
-                datapoints.points[j] = i;
+                datapoints.points[j] = start + (i * step);
                 datapoints.points[j + 1] = data[i];
             }
             datapoints.points.length = data.length * 2;
--- jquery.flot.js
+++ jquery.flot.js
@@ -2069,7 +2069,7 @@ Licensed under the MIT license.
                              pointsize: series.datapoints.pointsize};
 
            if (series.decimate) {
-               datapoints.points = series.decimate(datapoints.points, series.xaxis.min, series.xaxis.max, plotWidth);
+               datapoints.points = series.decimate(series, series.xaxis.min, series.xaxis.max, plotWidth);
            }
 
             var lw = series.lines.lineWidth,
