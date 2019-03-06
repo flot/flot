988582d3138c33301fe35c1a359e2c111ab7a836
jxcr0w <jxcr0w@protonmail.com>
2019-03-06 14:08:34 +0100

    Added microsecond support to jquery.flot.time.js

--- tests/jquery.flot.time.Test.js
+++ tests/jquery.flot.time.Test.js
@@ -24,20 +24,30 @@ describe('A Flot chart with absolute time axes', function () {
         return [arr[0], arr[arr.length - 1]];
     };
 
-    var createPlotWithAbsoluteTimeAxis = function (placeholder, data, formatString) {
+    var createPlotWithAbsoluteTimeAxis = function (placeholder, data, formatString, base) {
         return $.plot(placeholder, data, {
             xaxis: {
                 mode: 'time',
                 timeformat: formatString,
-                timeBase: 'millseconds',
+                timeBase: base,
                 showTickLabels: 'all'
             },
             yaxis: {}
         });
     };
 
+    var validateTickLabelFormat = function (ticks, regexFormat) {
+        var isFormatCorrect = true;
+        ticks.forEach(function (t) {
+            if (!t.label.match(regexFormat)) {
+                isFormatCorrect = false;
+            }
+        });
+        return isFormatCorrect;
+    };
+
     it('shows time ticks', function () {
-        plot = createPlotWithAbsoluteTimeAxis(placeholder, [[[0, 1], [1000, 2]]], '%Y/%m/%d %M:%S');
+        plot = createPlotWithAbsoluteTimeAxis(placeholder, [[[0, 1], [1000, 2]]], '%Y/%m/%d %M:%S', 'milliseconds');
 
         expect(firstAndLast(plot.getAxes().xaxis.ticks)).toEqual([
             {v: 0, label: '1970/01/01 00:00'},
@@ -53,6 +63,38 @@ describe('A Flot chart with absolute time axes', function () {
             expect(dateGenerator(8640000000000002, {}).date).toEqual(new Date(8640000000000000));
             expect(dateGenerator(-8640000000000001, {}).date).toEqual(new Date(-8640000000000000));
             expect(dateGenerator(-9640000000000000, {}).date).toEqual(new Date(-8640000000000000));
+        });
+    });
+
+    describe('timebase in microseconds', function () {
+        it('supports timestamps given in microseconds', function () {
+            plot = createPlotWithAbsoluteTimeAxis(placeholder, [[[0, 1], [1000000, 2]]], '%Y/%m/%d %M:%S', 'microseconds');
+
+            expect(firstAndLast(plot.getAxes().xaxis.ticks)).toEqual([
+                {v: 0, label: '1970/01/01 00:00'},
+                {v: 1000000, label: '1970/01/01 00:01'}
+            ]);
+        });
+    });
+
+    describe('sub-second ticks', function () {
+        it('formats ticks to microsecond accuracy', function () {
+            plot = createPlotWithAbsoluteTimeAxis(placeholder, [[[0, 1], [1000125, 2]]], '%H:%M:%S.%s', 'microseconds');
+
+            expect(validateTickLabelFormat(plot.getAxes().xaxis.ticks), /00:00:0\d\.\d{6}/).toEqual(true);
+            expect(firstAndLast(plot.getAxes().xaxis.ticks)).toEqual([
+                {v: 0, label: '00:00:00.000000'},
+                {v: 1000125, label: '00:00:01.000125'}
+            ]);
+        });
+        it('supports tick formatted to millisecond (3 decimals) accuracy', function () {
+            plot = createPlotWithAbsoluteTimeAxis(placeholder, [[[0, 1], [1002000, 2]]], '%H:%M:%S.%3s', 'microseconds');
+
+            expect(validateTickLabelFormat(plot.getAxes().xaxis.ticks), /00:00:0\d\.\d{3}/).toEqual(true);
+            expect(firstAndLast(plot.getAxes().xaxis.ticks)).toEqual([
+                {v: 0, label: '00:00:00.000'},
+                {v: 1002000, label: '00:00:01.002'}
+            ]);
         });
     });
 });
