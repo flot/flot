a92f998e24ad526493b218cdd0f8deccf01a920f
jxcr0w <jxcr0w@protonmail.com>
2019-03-12 15:47:10 +0100

    unified the test names, added quarter-second and quarter-millisecond cases

--- tests/jquery.flot.time.Test.js
+++ tests/jquery.flot.time.Test.js
@@ -24,12 +24,13 @@ describe('A Flot chart with absolute time axes', function () {
         return [arr[0], arr[arr.length - 1]];
     };
 
-    var createPlotWithAbsoluteTimeAxis = function (placeholder, data, formatString, base) {
+    var createPlotWithAbsoluteTimeAxis = function (placeholder, data, formatString, base, minTickSize) {
         return $.plot(placeholder, data, {
             xaxis: {
                 mode: 'time',
                 timeformat: formatString,
                 timeBase: base,
+                minTickSize: minTickSize,
                 showTickLabels: 'all'
             },
             yaxis: {}
@@ -81,20 +82,47 @@ describe('A Flot chart with absolute time axes', function () {
         it('formats ticks to microsecond accuracy', function () {
             plot = createPlotWithAbsoluteTimeAxis(placeholder, [[[0, 1], [1000125, 2]]], '%H:%M:%S.%s', 'microseconds');
 
-            expect(validateTickLabelFormat(plot.getAxes().xaxis.ticks), /00:00:0\d\.\d{6}/).toEqual(true);
+            expect(validateTickLabelFormat(plot.getAxes().xaxis.ticks, /00:00:0\d\.\d{6}/)).toEqual(true);
             expect(firstAndLast(plot.getAxes().xaxis.ticks)).toEqual([
                 {v: 0, label: '00:00:00.000000'},
                 {v: 1000125, label: '00:00:01.000125'}
             ]);
         });
-        it('supports tick formatted to millisecond (3 decimals) accuracy', function () {
+        it('formats ticks to millisecond (3 decimals) accuracy', function () {
             plot = createPlotWithAbsoluteTimeAxis(placeholder, [[[0, 1], [1002000, 2]]], '%H:%M:%S.%3s', 'microseconds');
 
-            expect(validateTickLabelFormat(plot.getAxes().xaxis.ticks), /00:00:0\d\.\d{3}/).toEqual(true);
+            expect(validateTickLabelFormat(plot.getAxes().xaxis.ticks, /00:00:0\d\.\d{3}/)).toEqual(true);
             expect(firstAndLast(plot.getAxes().xaxis.ticks)).toEqual([
                 {v: 0, label: '00:00:00.000'},
                 {v: 1002000, label: '00:00:01.002'}
             ]);
+        });
+        it('creates ticks automatically to microsecond accuracy', function () {
+            plot = createPlotWithAbsoluteTimeAxis(placeholder, [[[0, 1], [10, 2]]], null, 'microseconds');
+
+            expect(validateTickLabelFormat(plot.getAxes().xaxis.ticks, /00\.\d{6}/)).toEqual(true);
+            expect(firstAndLast(plot.getAxes().xaxis.ticks)).toEqual([
+                {v: 0, label: '00.000000'},
+                {v: 10, label: '00.000010'}
+            ]);
+        });
+        it('creates quarter-second timestaps', function () {
+            plot = createPlotWithAbsoluteTimeAxis(placeholder, [[[0, 1], [1, 2]]], null, 'seconds', [250, 'millisecond']);
+
+            expect(validateTickLabelFormat(plot.getAxes().xaxis.ticks, /0\d\.(00|25|50|75)/)).toEqual(true);
+            expect(firstAndLast(plot.getAxes().xaxis.ticks)).toEqual([
+                {v: 0, label: '00.00'},
+                {v: 1, label: '01.00'}
+            ])
+        });
+        it('creates quarter-millisecond timestaps', function () {
+            plot = createPlotWithAbsoluteTimeAxis(placeholder, [[[0, 1], [1, 2]]], null, 'milliseconds', [250, 'microsecond']);
+
+            expect(validateTickLabelFormat(plot.getAxes().xaxis.ticks, /00\.00\d(00|25|50|75)/)).toEqual(true);
+            expect(firstAndLast(plot.getAxes().xaxis.ticks)).toEqual([
+                {v: 0, label: '00.00000'},
+                {v: 1, label: '00.00100'}
+            ])
         });
     });
 });
