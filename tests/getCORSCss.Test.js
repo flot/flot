0f2e6cceeff83fe26674f90506da3ff02ef115a2
zhawang <494233757@qq.com>
2019-06-03 23:14:34 +0800

    add one expect item

--- tests/getCORSCss.Test.js
+++ tests/getCORSCss.Test.js
@@ -7,8 +7,10 @@ describe("getCrossDomainCSSRules", function() {
             throw new DOMException("Failed to read the 'cssRules' property from 'CSSStyleSheet': Cannot access rules", 'SecurityError');
         });
         expect(() => {document.styleSheets[2].cssRules;}).toThrow(new DOMException("Failed to read the 'cssRules' property from 'CSSStyleSheet': Cannot access rules", 'SecurityError'));
+        const styleSheetsCount = document.styleSheets.length;
         await getCrossDomainCSSRules(document);
         expect(document.styleSheets[2].ownerNode.crossOrigin).toBe('anonymous'); // should set crossOrigin attr successfully.
-        expect(document.styleSheets[1].ownerNode.crossOrigin).toBeNull(); //should not change if no exception threw.
+        expect(document.styleSheets[0].ownerNode.crossOrigin).toBeNull(); //should not change if no exception threw.
+        expect(document.styleSheets.length).toBe(styleSheetsCount); // the styleSheets length should be same after getCrossDomainCSSRules invoked.
     });
 });
