5ad32984972949de410df42c205bf06ed9bdb4a0
Brian Henry <brian.henry@ni.com>
2019-06-13 13:26:40 -0500

    Reverting changes for loading CORS stylesheets (PRs 1651 and 1652) (#1666)
    * Fixed microsecond issue.
    
    * Reverted changes associated with PRs 1651 and 1652
    
    * Update package-lock.json

--- gulpfile.js
+++ gulpfile.js
@@ -4,15 +4,9 @@ var uglify = require('gulp-uglify');
 var babel = require('gulp-babel');
 var filesExist = require('files-exist');
 var maps = require('gulp-sourcemaps');
-var babelify = require("babelify");
-var browserify = require('browserify');
 var gulpSequence = require('gulp-sequence');
-var source = require('vinyl-source-stream');
-var fs = require('fs');
-
 
 var files = [
-    './source/getCORSCss.js',
     './source/jquery.canvaswrapper.js',
     './source/jquery.colorhelpers.js',
     './source/jquery.flot.js',
@@ -38,64 +32,24 @@ var files = [
     './source/jquery.flot.legend.js'
 ];
 
-gulp.task('modify jquery.flot.composeImages.js first', async function() {
-    fs.readFile('./source/jquery.flot.composeImages.js', (err, data) => {
-        if (err) {
-            return console.log(err);
-        }
-        var content = data.toString();
-        var new_content = content.replace(`import {getCrossDomainCSSRules} from "./getCORSCss";`,`//import {getCrossDomainCSSRules} from "./getCORSCss";`);
-        fs.writeFile('./source/jquery.flot.composeImages.js', new_content, (err) => {
-            if (err) {
-                return console.error(err);
-            }
-        })
-    });
-});
-
 gulp.task('build_flot_source', function() {
     return gulp.src(filesExist(files, { exceptionMessage: 'Missing file' }))
         .pipe(concat('jquery.flot.js'))
         .pipe(gulp.dest('dist/source'));
 });
 
-gulp.task('build_flot_minified_start', function() {
+gulp.task('build_flot_minified', function() {
     return gulp.src(filesExist(files, { exceptionMessage: 'Missing file' }))
-        .pipe(concat('jquery.flot.js'))
-        .pipe(gulp.dest('dist/es5'));
-});
-
-gulp.task('browserify_flot', function() {
-    var b = browserify({
-        entries: "dist/es5/jquery.flot.js"
-    });
-    return b.transform(babelify, {presets: ["@babel/preset-env"]})
-        .bundle()
-        .pipe(source("jquery.flot.js"))
-        .pipe(gulp.dest("dist/es5"));
-});
-
-gulp.task('build_flot_minified_end', function() {
-    return gulp.src(filesExist("dist/es5/jquery.flot.js", { exceptionMessage: 'Missing file' }))
         .pipe(maps.init())
+        .pipe(babel({
+            "presets": [
+                "@babel/preset-env"
+            ]
+        }))
+        .pipe(concat('jquery.flot.js'))
         .pipe(uglify())
         .pipe(maps.write('./'))
         .pipe(gulp.dest('dist/es5'));
 });
 
-gulp.task('modify jquery.flot.composeImages.js in the end', async function() {
-    fs.readFile('./source/jquery.flot.composeImages.js', (err, data) => {
-        if (err) {
-            return console.log(err);
-        }
-        var content = data.toString();
-        var new_content = content.replace(`//import {getCrossDomainCSSRules} from "./getCORSCss";`,`import {getCrossDomainCSSRules} from "./getCORSCss";`);
-        fs.writeFile('./source/jquery.flot.composeImages.js', new_content, (err) => {
-            if (err) {
-                return console.error(err);
-            }
-        })
-    });
-});
-
-gulp.task('build', gulp.series('modify jquery.flot.composeImages.js first', 'build_flot_source', 'build_flot_minified_start', 'browserify_flot', 'build_flot_minified_end', 'modify jquery.flot.composeImages.js in the end'));
+gulp.task('build', gulp.series('build_flot_source', 'build_flot_minified'));
