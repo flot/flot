568b61e8b4695c325f31d0fef27527a4ac590498
Jon Daley <jdaley@medallia.com>
2023-11-08 12:58:12 -0500

    added changes from https://stackoverflow.com/questions/9496689/overlapping-labels-in-flot-pie-chart

--- source/jquery.flot.pie.js
+++ source/jquery.flot.pie.js
@@ -448,6 +448,7 @@ More detail and specific examples can be found in the included HTML file.
                 }
 
                 function drawLabels() {
+                    var labels = [];
                     var currentAngle = startAngle;
                     var radius = options.series.pie.label.radius > 1 ? options.series.pie.label.radius : maxRadius * options.series.pie.label.radius;
 
@@ -493,6 +494,35 @@ More detail and specific examples can be found in the included HTML file.
 
                         label.css("top", labelTop);
                         label.css("left", labelLeft);
+
+                        // check to make sure that the label doesn't overlap one of the other labels
+                        var label_pos = getPositions(label);
+                        for(var j=0; j<labels.length; j++) {
+                            var tmpPos = getPositions(labels[j]);
+                            var horizontalMatch = comparePositions(label_pos[0], tmpPos[0]);
+                            var verticalMatch = comparePositions(label_pos[1], tmpPos[1]);
+                            var match = horizontalMatch && verticalMatch;
+                            if(match) {
+                                var newTop = tmpPos[1][0] - (label.height() + 1);
+                                label.css('top', newTop);
+                                labelTop = newTop;
+                            }
+                        }
+
+                        function getPositions(box) {
+                            var $box = $(box);
+                            var pos = $box.position();
+                            var width = $box.width();
+                            var height = $box.height();
+                            return [ [ pos.left, pos.left + width ], [ pos.top, pos.top + height ] ];
+                        }
+
+                        function comparePositions(p1, p2) {
+                            var x1 = p1[0] < p2[0] ? p1 : p2;
+                            var x2 = p1[0] < p2[0] ? p2 : p1;
+                            return x1[1] > x2[0] || x1[0] === x2[0] ? true : false;
+                        }
+                        labels.push(label);
 
                         // check to make sure that the label is not outside the canvas
                         if (0 - labelTop > 0 || 0 - labelLeft > 0 || canvasHeight - (labelTop + label.height()) < 0 || canvasWidth - (labelLeft + label.width()) < 0) {
